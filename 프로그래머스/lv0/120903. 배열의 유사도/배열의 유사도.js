09977145050176d56a971da4058583f285d4ea8d
HyeBeen <104710243+HYBEN09@users.noreply.github.com>
2023-05-18 17:11:28 +0900

    [level 0] Title: 배열의 유사도, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,6 +1,20 @@
-function solution(s1, s2) {
-   const result = s1.filter(item => s2.includes(item))
+// function solution(s1, s2) {
+//    const result = s1.filter(item => s2.includes(item))
    
-   return result.length
+//    return result.length
     
+// }
+
+function solution(s1, s2) {
+  const set1 = new Set(s1); // 배열 s1의 원소를 중복 없이 저장하는 Set 생성
+  const set2 = new Set(s2); // 배열 s2의 원소를 중복 없이 저장하는 Set 생성
+  let count = 0; // 같은 원소의 개수를 저장할 변수
+
+  for (const item of set1) {
+    if (set2.has(item)) {
+      count++;
+    }
+  }
+
+  return count;
 }

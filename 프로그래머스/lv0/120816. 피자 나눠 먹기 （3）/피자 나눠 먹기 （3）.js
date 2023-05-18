12844b2aa1c5d7f7831f1f90ed9b0fc5038b8944
HyeBeen <104710243+HYBEN09@users.noreply.github.com>
2023-05-18 11:28:28 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
@@ -1,4 +1,11 @@
+// function solution(slice, n) {
+//     let pizza = n % slice === 0 ? n/slice : Math.floor(n / slice) +1
+//     return pizza
+// }
+
+
+// 다른 풀이
+
 function solution(slice, n) {
-    let pizza = n % slice === 0 ? n/slice : Math.floor(n / slice) +1
-    return pizza
+    return Math.ceil(n / slice)
 }

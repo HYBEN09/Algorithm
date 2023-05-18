dfac7690b61c94641f3ed67e7d745a03adb80f1d
HyeBeen <104710243+HYBEN09@users.noreply.github.com>
2023-05-18 11:19:12 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -11,3 +11,11 @@ const solution = (dot) => {
     return 4;
   }
 };
+
+
+
+// function solution(dot) {
+//     const [num,num2] = dot;
+//     const check = num * num2 > 0;
+//     return num > 0 ? (check ? 1 : 4) : (check ? 3 : 2);
+// }

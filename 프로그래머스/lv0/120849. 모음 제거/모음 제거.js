b0b859d78e1b014c20ddc12db604750a272f0e98
HyeBeen <104710243+HYBEN09@users.noreply.github.com>
2023-05-18 16:18:41 +0900

    [level 0] Title: 모음 제거, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
@@ -1,11 +1,17 @@
-function solution(my_string) {
-    const vowels = ["a", "e", "i", "o", "u"]
-    let results = ''
+// function solution(my_string) {
+//     const vowels = ["a", "e", "i", "o", "u"]
+//     let results = ''
     
-    for(let key of my_string){
-        if(!vowels.includes(key)){
-            results += key
-        }
-    }
-    return results
+//     for(let key of my_string){
+//         if(!vowels.includes(key)){
+//             results += key
+//         }
+//     }
+//     return results
+// }
+
+//다른 풀이
+
+function solution(my_string) {
+    return my_string.replace(/[aeiou]/g, '');
 }

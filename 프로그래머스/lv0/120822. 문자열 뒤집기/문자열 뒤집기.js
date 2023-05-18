e63f767f351ecbbb80854eb8c15e924a35609856
HyeBeen <104710243+HYBEN09@users.noreply.github.com>
2023-05-18 15:59:49 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -0,0 +1,7 @@
+function solution(my_string) {
+    let splitString = my_string.split("")
+    let reverseString = splitString.reverse()
+    let joinString = reverseString.join("")
+    
+    return joinString
+}

1a1d967805789c21eb67bb2ce347d8db3230d1da
HyeBeen <104710243+HYBEN09@users.noreply.github.com>
2023-05-18 11:45:48 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -0,0 +1,6 @@
+function solution(my_string, letter) {
+   // replace() 메서드에 정규표현식과 g 플래그를 사용하여 모든 일치하는 부분을 제거
+    const regex = new RegExp(letter, 'g');
+    const result = my_string.replace(regex, '');
+    return result
+}

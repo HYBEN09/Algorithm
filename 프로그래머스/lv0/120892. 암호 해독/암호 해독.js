f755eb79f5a0e78951648c91e741170eb809fdb2
HyeBeen <104710243+HYBEN09@users.noreply.github.com>
2023-05-19 11:11:56 +0900

    [level 0] Title: 암호 해독, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
@@ -3,6 +3,7 @@ function solution(cipher, code) {
     let result = ''
     
     // i 변수를 code - 1로 초기화하고, i를 code씩 증가시키면서 반복
+    // 첫 번째 글자를 추출하기 위해서는 인덱스 0을 사용
     for(let i = code - 1; i < cipher.length; i += code){
         result += cipher[i]
     }

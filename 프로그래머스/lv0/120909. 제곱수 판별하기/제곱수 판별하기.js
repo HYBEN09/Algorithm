d7c0adba34a7a5621361fa19a3bef7f52102c96e
HyeBeen <104710243+HYBEN09@users.noreply.github.com>
2023-05-19 10:21:08 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.js
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.js
@@ -3,3 +3,11 @@ function solution(n) {
    
    return sqrt * sqrt === n ? 1 : 2
 }
+
+//다른 풀이
+function solution(n) {
+  const sqrt = Math.sqrt(n);
+  
+  //Number.isInteger : 제곱근이 정수인지 확인
+  return Number.isInteger(sqrt) ? 1 : 2;
+}

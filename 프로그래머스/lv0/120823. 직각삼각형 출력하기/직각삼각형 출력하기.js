89866a21a23d33eaf283605a27884e1537dfae16
HyeBeen <104710243+HYBEN09@users.noreply.github.com>
2023-05-22 23:39:05 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 50.46 ms, Memory: 32.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.js
+++ 프로그래머스/lv0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.js
@@ -1,3 +1,23 @@
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
 const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
@@ -9,13 +29,11 @@ let input = [];
 rl.on('line', function (line) {
     input = line.split(' ');
 }).on('close', function () {
-    const num = Number(input[0]);
-    let logStr = ''
-    for(let i = 0 ; i < num; i++) {
-        for(let j = 0 ; j <= i; j++) {
-            logStr+='*'
-        }
-        logStr+='\n'
-    }
-    console.log(logStr)
+    solution(Number(input[0]));
 });
+
+function solution(n) {
+    for(let i = 1; i < n + 1; i++) {
+        console.log('*'.repeat(i));
+    }
+}

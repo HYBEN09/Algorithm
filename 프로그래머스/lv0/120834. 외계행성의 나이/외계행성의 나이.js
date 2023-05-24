4379588308a51a53890edcad8ae679410c1ff409
HyeBeen <104710243+HYBEN09@users.noreply.github.com>
2023-05-24 23:38:34 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -10,3 +10,13 @@ function solution(age) {
 
   return result;
 }
+
+//다른 풀이 -------------------------------------------------
+
+function solution(age) {
+  return age
+    .toString()
+    .split("")
+    .map((v) => "abcdefghij"[v])
+    .join("");
+}

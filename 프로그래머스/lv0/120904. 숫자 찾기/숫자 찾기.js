0723dfa7e9ddd69f8b89997f6b0441ca3434664d
HyeBeen <104710243+HYBEN09@users.noreply.github.com>
2023-08-14 10:34:11 +0900

    [level 0] Title: 숫자 찾기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.js
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.js
@@ -1,11 +1,5 @@
+
 function solution(num, k) {
-  const numStr = num.toString()  // 정수를 문자열로 변환
-  const kStr = k.toString()
-  
-  for(let i = 0; i<numStr.length; i++){
-       if (numStr[i] === kStr) {
-       return i + 1;
-       }
-  }
-    return -1 // k를 찾지 못한 경우
+    return num.toString().split("").map((el) => Number(el)).indexOf(k) + 1 || -1
+    
 }
